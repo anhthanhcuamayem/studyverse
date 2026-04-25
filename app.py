8f99d9361c715955ed8eba02269b1505f99b778b
anhthanhcuamayem <nguyenduocthanh2008@gmail.com>
2026-04-25 14:03:45 +0700

    Update Flask app to fix CSS rendering issue
    Fix CSS rendering issue by updating static folder path.

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
-app = Flask(__name__,static_folder='static')
+import os
+
+app = Flask(__name__)
+
+# THÊM DÒNG NÀY ĐỂ FIX LỖI CSS TRÊN RENDER
+app._static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 
 @app.route('/')
 def home():
@@ -7,11 +12,11 @@ def home():
 
 @app.route('/mylist')
 def mylist():
-    return render_template('mylist.html') # Bạn phải có file này trong folder templates
+    return render_template('mylist.html')
 
 @app.route('/create')
 def create():
-    return render_template('create.html') # Bạn phải có file này trong folder templates
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
