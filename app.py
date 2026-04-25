27a6eceda81e846f790437cda769c0f81e69d880
duybao <2009nguyenduybao@gmail.com>
2026-04-25 13:36:22 +0700

    app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def home():
 
 @app.route('/mylist')
 def mylist():
-    return render_template('index.html')
+    return render_template('mylist.html') # Bạn phải có file này trong folder templates
 
 @app.route('/create')
 def create():
-    return render_template('index.html')
+    return render_template('create.html') # Bạn phải có file này trong folder templates
 
 if __name__ == '__main__':
     app.run(debug=True)
